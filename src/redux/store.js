b14e0d3e16986df72daa6166df94b6bd3e64bba3
sjkings1 <samsonjarald111@gmail.com>
2022-02-01 15:56:35 +0530

    newUpdate

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,6 @@
-import {createStore} from 'redux'
+import {createStore} from 'redux';
 import { rootReducer } from "./rootReducer";
-import {composeWithDevTools} from 'redux-devtools-extension'
+import {composeWithDevTools} from 'redux-devtools-extension';
 
 let store = createStore(rootReducer, composeWithDevTools())
 
